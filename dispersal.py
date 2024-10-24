bb97180bf360b2073c0a4562bee87bd83a5b8ec7
Douglas Cortes <135165192+dxara10@users.noreply.github.com>
2024-10-24 17:00:34 -0300

    Create dispersal.py

---
+++ dispersal.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+# Dados
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+# Criar gráfico de dispersão
+plt.scatter(x, y)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Gráfico de Dispersão')
+plt.show()
